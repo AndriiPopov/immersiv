a126b79f534ec104e78d0c4ba406179908261c50
AndriiPopov <andriy.popov.vl@gmail.com>
2022-02-25 10:07:16 +0100

    Update

--- controllers/constant.controller.js
+++ controllers/constant.controller.js
@@ -2,7 +2,7 @@ const constantService = require("../services/constant.service");
 
 const updateConstant = async (req, res) => {
     const updatedData = await constantService.updateConstant(req.body);
-    if (data.length) res.status(200).json(data[0]);
+    if (updatedData.length) res.status(200).json(updatedData[0]);
     else res.status(500).send();
 };
 
