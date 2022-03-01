3a421c15ae26c9f12e7602f1f58d6e00379f460e
AndriiPopov <andriy.popov.vl@gmail.com>
2022-03-01 12:52:22 +0100

    Update

--- services/mail.service.js
+++ services/mail.service.js
@@ -35,7 +35,7 @@ const html = require("../helpers/signup");
 
 const url =
     process.env.NODE_ENV === "production"
-        ? "https://www.immersiv.com.au"
+        ? "https://tour.immersiv.com.au"
         : "http://localhost:3000";
 
 const signupMail = (to, name) => {
