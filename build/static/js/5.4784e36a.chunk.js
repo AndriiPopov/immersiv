35d675b20748ddb9fc390845b0ac4999e7fd8229
AndriiPopov <andriy.popov.vl@gmail.com>
2022-04-04 07:41:24 +0200

    Fix ios chart

---
+++ build/static/js/5.4784e36a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[5],{1213:function(t,e,n){"use strict";n.r(e);n(0);var c=n(1204),s=n(6);e.default=function(){return Object(s.jsx)(c.default,{featured:!0})}}}]);
+//# sourceMappingURL=5.4784e36a.chunk.js.map
