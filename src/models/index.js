c4a32054f48cac8dd067af133cf99e7c0a401baf
AndriiPopov <andriy.popov.vl@gmail.com>
2022-08-16 22:48:50 -0600

    Test

--- src/models/index.js
+++ src/models/index.js
@@ -52,9 +52,9 @@ db.sequelize
     .catch((err) => {})
 
 db.projectModel.update({ media: [] }, { where: { media: null } })
-db.propertyModel.update({ Frontage: 0 }, { where: { Frontage: null } })
-db.propertyModel.update({ Depth: 0 }, { where: { Depth: null } })
-db.propertyModel.update({ Depth: 0 }, { where: { Depth: null } })
+// db.propertyModel.update({ Frontage: 0 }, { where: { Frontage: null } })
+// db.propertyModel.update({ Depth: 0 }, { where: { Depth: null } })
+// db.propertyModel.update({ Depth: 0 }, { where: { Depth: null } })
 
 db.adminModel.findOrCreate({
     where: { email: 'andriy.popov.vl@gmail.com' },
