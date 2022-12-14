01ce3c4f9d2b45e51c5cf22d0386da5f12e72d4e
Andrii Popov <andriy.popov.vl@gmail.com>
2022-12-14 17:15:07 -0600

    d

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(compression())
 app.use(cookieParser())
 
 app.use('/api', routes)
-app.use('/gallery', express.static(path.join(__dirname, 'buildGallery')))
+// app.use('/gallery', express.static(path.join(__dirname, 'buildGallery')))
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
